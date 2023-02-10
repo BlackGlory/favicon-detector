0173a86bd329e439222ff0cd48e118c84127507f
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-02-10 15:53:00 +0800

    fix: add css reset

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as ReactDOM from 'react-dom'
+import 'antd/dist/reset.css'
 import { Popup } from '@components/popup'
 
 ReactDOM.render(<Popup />, document.querySelector('main'))
